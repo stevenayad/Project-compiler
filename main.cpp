9fb0d1feb4b4731e62d43bf845f6411d2bf25bf4
steven ayad <stevenayad9@gmail.com>
2025-08-22 09:29:26 -0500

    Merge Evalution with main

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 #include "Evalution.cpp"
 using namespace std;
 
-
 int main() {
     ifstream in("input.txt");
     ofstream tok("tokens.txt");
@@ -15,45 +14,50 @@ int main() {
     if (!in || !tok) {
         cout << "Error opening files Scanner.\n";
         return 1;
-    }
-    else {
+    } else {
         cout << "Files open successfully Scanner.\n";
     }
 
-    string expression; 
+    
+    string expression;
     string line;
     while (getline(in, line)) {
         if (!line.empty()) {
-            expression += line;  
-            scan(line, tok);     
+            expression += line;
+            scan(line, tok);   
         }
     }
 
     tok.close();
     in.close();
 
+   
     parser p;
     p.loadTremails("termails.txt");
     p.loadnonTremails("nontermails.txt");
     p.loadTokens("tokens.txt");
     p.loadpredectivetable("predectivetable.txt");
-    
+
     bool success = p.parse("E");
 
     if (success) {
+        cout << "Parsing successful" << endl;
+
         ExpressionConverter conv;
+
+        
         string postfix = conv.infixToPostfix(expression);
+        
+
         try {
             double value = conv.Evalation(postfix);
-            cout << "Evaluation: " << value << endl;
-        } catch (const std::exception& e) {
+            cout << "Evaluation Result = " << value << endl;
+        } catch (const exception &e) {
             cerr << "Evaluation error: " << e.what() << endl;
         }
-    }
-    else {
-       cout << "Skipping postfix conversion and evaluation because parsing failed." << endl;
+    } else {
+        cout << "Parsing failed. Skipping postfix conversion and evaluation." << endl;
     }
 
-    
     return 0;
 }
